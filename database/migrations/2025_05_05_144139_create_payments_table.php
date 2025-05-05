9d7d6117bd60db125db5c03835bf2824dc2cf32d
rizkiiaripin <rizkixirpl@gmail.com>
2025-05-05 22:27:41 +0700

    menghapus table blogs

--- database/migrations/2025_05_05_144139_create_payments_table.php
+++ database/migrations/2025_05_05_144139_create_payments_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount',12 , 2);
+            $table->string('method');
+            $table->string('status');
             $table->timestamps();
         });
     }
