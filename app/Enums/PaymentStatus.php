08b5c77d90d329f820ce533aa685a478214a0a6f
rizkiiaripin <rizkixirpl@gmail.com>
2025-05-05 22:51:01 +0700

    mengubah urutan migration

--- app/Enums/PaymentStatus.php
+++ app/Enums/PaymentStatus.php
@@ -2,7 +2,27 @@
 
 namespace App\Enums;
 
-enum PaymentStatus
+enum PaymentStatus : string
 {
-    //
+    case Unpaid     = 'unpaid';      // Belum dibayar
+    case Pending    = 'pending';     // Menunggu konfirmasi dari payment gateway
+    case Paid       = 'paid';        // Sudah dibayar
+    case Failed     = 'failed';      // Gagal bayar (misalnya error saat transaksi)
+    case Refunded   = 'refunded';    // Dana sudah dikembalikan
+    case Expired    = 'expired';     // Waktu pembayaran habis
+    case Cancelled  = 'cancelled';   // Dibatalkan sebelum dibayar
+
+    // Label Bahasa Indonesia
+    public function label(): string
+    {
+        return match($this) {
+            self::Unpaid    => 'Belum Dibayar',
+            self::Pending   => 'Menunggu Konfirmasi',
+            self::Paid      => 'Sudah Dibayar',
+            self::Failed    => 'Gagal Pembayaran',
+            self::Refunded  => 'Dana Dikembalikan',
+            self::Expired   => 'Kadaluarsa',
+            self::Cancelled => 'Dibatalkan',
+        };
+    }
 }
--- database/migrations/2025_05_05_144139_create_payments_table.php
+++ database/migrations/2025_05_05_144139_create_payments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\PaymentStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->decimal('amount',12 , 2);
             $table->string('method');
-            $table->string('status');
+            $table->string('status')->default(PaymentStatus::Pending);
             $table->timestamps();
         });
     }
